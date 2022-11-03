7102f0a375365934af336c1ec2fc1b8ae5f4214c
wesleyara <wes1gom@gmail.com>
2022-11-03 10:51:23 -0300

    feat: adicionando tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    display: ["Poppins", "system-ui", "sans-serif"],
+    body: ["Poppins", "system-ui", "sans-serif"],
+    extend: {
+      colors: {
+        brand: {
+          black: "var(--black-color)",
+        },
+      },
+      backgroundColor: {
+        primary: "var(--bg-primary)",
+        secondary: "var(--bg-secondary)",
+      },
+      textColor: {
+        primary: "var(--text-primary)",
+        secondary: "var(--text-secondary)",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          xl: "0rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
